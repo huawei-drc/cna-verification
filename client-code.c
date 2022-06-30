9f064a97e52c0ff02cfe33ff0cadfbb532fad74f
Diogo Behrens <diogo.behrens@huawei.com>
2022-06-30 18:47:04 +0200

    several updates to support dartagnan
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- client-code.c
+++ client-code.c
@@ -2,15 +2,25 @@
 // SPDX-License-Identifier: MIT
 
 /*******************************************************************************
- * Run this client code with GenMC 0.6.1 to verify the correctness of the
+ * Run this client code with GenMC 0.7 to verify the correctness of the
  * CNA slowpath of qspinlock (qspinlock_cna.h) from Linux 5.14.
  * The client code can be alternatively configured to use the MCS slowpath or
- * even just the plain MCS lock (mcs_spinlock.h).
+ * just the plain MCS lock (mcs_spinlock.h).
  ******************************************************************************/
 
 /* Number of threads */
 #ifndef NTHREADS
 #define NTHREADS 4
+#endif
+
+/* Number of threads that reacquire the lock (once) */
+#ifndef REACQUIRE
+#define REACQUIRE 0
+#endif
+
+/* Number of reacquires for each thread that does reacquires */ 
+#ifndef REPEAT
+#define REPEAT 1
 #endif
 
 /* Supported algorithms */
@@ -21,25 +31,50 @@
 #define ALGORITHM     QSPINLOCK_CNA
 #endif
 
+/* skip fast path and pending logic, jump directly to queue */
+//#define SKIP_PENDING
+
 /* GenMC can check liveness only if it knows the loops that depend on updates
  * from other threads (spinloops). Its automated spinloop detection does not
  * always work, in particular with these files. So we manually annotate the
  * spinloops. The annotation can be disabled by commenting the next line.
  * If SPIN_ANNOTATION is defined, please pass -disable-spin-assume to GenMC.
- */
+ */ 
 #define SPIN_ANNOTATION
+
+/* GenMC runs out of memory if cond_load_relaxed are really relaxed. Therefore,
+ * we replace them by default with cond_load_acquire.
+ * Define COND_LOAD_RLX to disable that */
+#define COND_LOAD_RLX
+#ifndef COND_LOAD_RLX 
+#define COND_LOAD_ACQUIRE
+#endif
 
 /*******************************************************************************
  * Includes, context, lock selection -- NO USER OPTIONS FROM HERE ON.
  ******************************************************************************/
+#define VERIFICATION
+
 #include <assert.h>
 #include <pthread.h>
 #include <stddef.h>
 #include <stdbool.h>
 
+/* await_while annotation */
+#ifdef SPIN_ANNOTATION
+  void __VERIFIER_loop_begin(void);
+  void __VERIFIER_spin_start(void);
+  void __VERIFIER_spin_end(bool);
+  void __VERIFIER_loop_bound(int);
+  #define await_while(cond) for (__VERIFIER_loop_begin();			\
+	__VERIFIER_spin_start(), (cond) ? 1 : (__VERIFIER_spin_end(1), 0);	\
+	__VERIFIER_spin_end(0))
+#else
+  #define await_while(cond) while(cond)
+#endif
+
 /* includes distributed in this repository */
 #include <linux/atomic.h> /* a mapping of linux atomic to GenMC functions */
-#include <await_while.h>  /* spinloop annotations */
 #include <defs.h>         /* replacement of several Linux macros */
 
 /* smp_processor_id */
@@ -47,7 +82,7 @@ __thread int tid;
 #define smp_processor_id() tid
 
 /* functions and macros to retrieve mcs/cna node (context) */
-void *get_node(int cpu);
+static void *get_node(int cpu);
 #define per_cpu_ptr(p, cpu) get_node(cpu)
 #define this_cpu_ptr(p) get_node(tid)
 #define __this_cpu_dec(x) ((struct mcs_spinlock*) this_cpu_ptr(x))->count--;
@@ -60,57 +95,78 @@ static bool cna_threshold_reached = false;
 #endif
 
 /* include qspinlock / CNA code */
+#define CONFIG_NR_CPUS NTHREADS
 #include <asm-generic/qspinlock.h>
 #include "kernel/locking/qspinlock.c"
 
 /* select lock and context */
 #if ALGORITHM == QSPINLOCK_CNA
-  struct qspinlock lock;
-  struct cna_node nodes[NTHREADS];
-  #define init()    cna_init_nodes();
-  #define nondet()  WRITE_ONCE(cna_threshold_reached, true);
-  #define acquire() queued_spin_lock_slowpath(&lock, 0)
-  #define release() queued_spin_unlock(&lock)
+	struct qspinlock lock;
+	/* use this instead of qnodes to make type clear in debugging */
+	struct cna_node nodes[NTHREADS];
+	#define init()    cna_init_nodes();
+	#define nondet()  WRITE_ONCE(cna_threshold_reached, true);
+#ifdef SKIP_PENDING
+	#define acquire() queued_spin_lock_slowpath(&lock, 0)
+#else
+	#define acquire() queued_spin_lock(&lock)
+#endif /* SKIP_PENDING */
+	#define release() queued_spin_unlock(&lock)
 #elif ALGORITHM == QSPINLOCK_MCS
-  struct qspinlock lock;
-  struct mcs_spinlock nodes[NTHREADS];
-  #define init()
-  #define nondet()
-  #define acquire() queued_spin_lock_slowpath(&lock, 0)
-  #define release() queued_spin_unlock(&lock)
+	struct qspinlock lock;
+	#define nodes (*qnodes)
+	#define init()
+	#define nondet()
+#ifdef SKIP_PENDING
+	#define acquire() queued_spin_lock_slowpath(&lock, 0)
+#else
+	#define acquire() queued_spin_lock(&lock)
+#endif /* SKIP_PENDING */
+	#define release() queued_spin_unlock(&lock)
 #else /* ALGORITHM == MCS_SPINLOCK */
-  struct mcs_spinlock *lock;
-  struct mcs_spinlock nodes[NTHREADS];
-  #define init()
-  #define nondet()
-  #define acquire() mcs_spin_lock(&lock, get_node(tid))
-  #define release() mcs_spin_unlock(&lock, get_node(tid))
+	struct mcs_spinlock *lock;
+	struct mcs_spinlock nodes[NTHREADS];
+	#define init()
+	#define nondet()
+	#define acquire() mcs_spin_lock(&lock, get_node(tid))
+	#define release() mcs_spin_unlock(&lock, get_node(tid))
 #endif
-void *get_node(int cpu) { return &nodes[cpu]; }
+static void *get_node(int cpu) { return &nodes[cpu]; }
 
 /*******************************************************************************
- * Actual client code
+ * Client code
  ******************************************************************************/
 static int x = 0, y = 0;
 static void* run(void *arg)
 {
-    tid = (intptr_t)arg;
-    acquire();
-    WRITE_ONCE(x, READ_ONCE(x)+1);
-    WRITE_ONCE(y, READ_ONCE(y)+1);
-    release();
-    return NULL;
+	tid = (intptr_t)arg;
+//	int rpt = REPEAT;
+//again:
+	acquire();
+	WRITE_ONCE(x, READ_ONCE(x)+1); /* GenMC has issues here */
+	WRITE_ONCE(y, READ_ONCE(y)+1); /* if these are plain accesses. */
+	release();
+//	if (tid < REACQUIRE && rpt--)
+//		goto again;
+	return NULL;
 }
 
 int main()
 {
-    pthread_t t[NTHREADS];
-    init();
-    for (intptr_t i = 0; i < NTHREADS; i++)
-        pthread_create(t+i, 0, run, (void*)i);
+	pthread_t t0, t1, t2, t3;
+	init();
+    pthread_create(&t0, 0, run, (void*)0);
+    pthread_create(&t1, 0, run, (void*)1);
+    pthread_create(&t2, 0, run, (void*)2);
+    pthread_create(&t3, 0, run, (void*)3);
+
     nondet();
-    for (intptr_t i = 0; i < NTHREADS; i++)
-        pthread_join(t[i], NULL);
-    assert (x == y && x == NTHREADS);
-    return 0;
+
+    pthread_join(t0, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
+    assert (READ_ONCE(x) == READ_ONCE(y));
+	return 0;
 }
--- include/linux/atomic.h
+++ include/linux/atomic.h
@@ -1,49 +1,71 @@
 #ifndef ATOMIC_H
 #define ATOMIC_H
 
-#ifdef MOCK_LKMM
-  #include "lkmm-mock.h"
-  #define smp_acquire__after_ctrl_dep() __atomic_thread_fence(__ATOMIC_ACQUIRE)
-#else
-  #include <genmc/lkmm.h>
-  #define smp_acquire__after_ctrl_dep() smp_rmb()
+#ifdef MOCK_LKMM /* to debug issues with LKMM mapping of genmc while using IMM */
+	#include "lkmm-mock.h"
+//	#define atomic_or(i, v)      ((void)__atomic_fetch_or(&(v)->counter, i, __ATOMIC_RELAXED))
+//	#define atomic_andnot(i, v)  ((void)__atomic_fetch_and(&(v)->counter, ~(i), __ATOMIC_RELAXED))
+#else   /* fixes for GenMC 0.8 */
+//	#include <genmc_internal.h>
+	#include <lkmm.h>
+	#define smp_acquire__after_ctrl_dep() smp_rmb()
+//	#undef atomic_cmpxchg_relaxed
+//	#define atomic_cmpxchg_relaxed(x, o, n) cmpxchg_relaxed(&(x)->counter, (int) o, n)
+//	#define atomic_fetch_or_acquire(i, v)  __atomic_fetch_or(&(v)->counter, i, memory_order_acquire)
+//	#define atomic_or(i, v)  do {							\
+//		__VERIFIER_atomicrmw_noret();						\
+//		atomic_fetch_or_explicit(&(v)->counter, i, memory_order_relaxed);	\
+//	} while(0)
+//	#define atomic_andnot(i, v) do {						\
+//		(void) __VERIFIER_atomicrmw_noret();					\
+//		atomic_fetch_and_explicit(&(v)->counter, ~(i), memory_order_relaxed);	\
+//	} while(0)
 #endif
-#define atomic_try_cmpxchg_acquire(x, o, n) ((int) *o == cmpxchg_acquire(&(x)->counter, (int) *o, n))
-#define atomic_try_cmpxchg_relaxed(x, o, n) ((int) *o == cmpxchg_relaxed(&(x)->counter, (int) *o, n))
-#define atomic_try_cmpxchg_release(x, o, n) ((int) *o == cmpxchg_release(&(x)->counter, (int) *o, n))
-#define atomic_try_cmpxchg(x, o, n)         ((int) *o == cmpxchg(&(x)->counter, (int) *o, n))
 
-#define atomic_fetch_or_acquire(i, v) __atomic_fetch_or(&(v)->counter, i, __ATOMIC_ACQUIRE)
 
+#define __atomic_try_cmpxchg(x, o, n, mo) ({         \
+	typeof(*o) r;                                \
+	r = cmpxchg##mo(&(x)->counter, (int) *o, n); \
+	(r == *o) ? 1 : (*o = r, 0);                 \
+})
+#define atomic_try_cmpxchg_acquire(x, o, n) __atomic_try_cmpxchg(x, o, n, _acquire)
+#define atomic_try_cmpxchg_relaxed(x, o, n) __atomic_try_cmpxchg(x, o, n, _relaxed)
+#define atomic_try_cmpxchg_release(x, o, n) __atomic_try_cmpxchg(x, o, n, _release)
+#define atomic_try_cmpxchg(x, o, n)         __atomic_try_cmpxchg(x, o, n,)
+ 
 #define __scalar_type_to_expr_cases(type)                               \
-                unsigned type:  (unsigned type)0,                       \
-                signed type:    (signed type)0
+		unsigned type:  (unsigned type)0,                       \
+		signed type:    (signed type)0
 
 #define __unqual_scalar_typeof(x) typeof(                               \
-                _Generic((x),                                           \
-                         char:  (char)0,                                \
-                         __scalar_type_to_expr_cases(char),             \
-                         __scalar_type_to_expr_cases(short),            \
-                         __scalar_type_to_expr_cases(int),              \
-                         __scalar_type_to_expr_cases(long),             \
-                         __scalar_type_to_expr_cases(long long),        \
-                         default: (x)))
+		_Generic((x),                                           \
+			 char:  (char)0,                                \
+			 __scalar_type_to_expr_cases(char),             \
+			 __scalar_type_to_expr_cases(short),            \
+			 __scalar_type_to_expr_cases(int),              \
+			 __scalar_type_to_expr_cases(long),             \
+			 __scalar_type_to_expr_cases(long long),        \
+			 default: (x)))
 
-#define smp_cond_load_relaxed(ptr, cond_expr) ({              \
-      typeof(ptr) __PTR = (ptr);                              \
-      __unqual_scalar_typeof(*ptr) VAL;                       \
-      await_do {                                              \
-              VAL = READ_ONCE(*__PTR); 	                      \
-	         } while_await (!cond_expr);                        \
-      (typeof(*ptr))VAL;                                      \
+/* this is the original macro from the Linux tree */
+#define smp_cond_load_relaxed(ptr, cond_expr) ({        \
+      typeof(ptr) __PTR = (ptr);                        \
+      __unqual_scalar_typeof(*ptr) VAL;                 \
+      for (;;) {                                        \
+          VAL = READ_ONCE(*__PTR);                      \
+          if (cond_expr)                                \
+              break;                                    \
+          cpu_relax();                                  \
+      }                                                 \
+      (typeof(*ptr))VAL;                                \
 })
 
-
-#define smp_cond_load_acquire(ptr, cond_expr) ({                \
-        __unqual_scalar_typeof(*ptr) _val;                      \
-        _val = smp_cond_load_relaxed(ptr, cond_expr);           \
-        smp_acquire__after_ctrl_dep();                          \
-        (typeof(*ptr))_val;                                     \
+/* this is the original macro from the Linux tree */
+#define smp_cond_load_acquire(ptr, cond_expr) ({        \
+      __unqual_scalar_typeof(*ptr) _val;                \
+      _val = smp_cond_load_relaxed(ptr, cond_expr);     \
+      smp_acquire__after_ctrl_dep();                    \
+      (typeof(*ptr))_val;                               \
 })
 
 #define atomic_cond_read_relaxed(ptr, cond_expr) smp_cond_load_relaxed(&(ptr)->counter, cond_expr)
