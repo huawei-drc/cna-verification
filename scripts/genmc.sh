2987fea3cdda87b88ee7892fd1f476c657d787b4
Diogo Behrens <diogo.behrens@huawei.com>
2022-07-06 09:19:36 +0200

    update scripts
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ scripts/genmc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ -z "${DOCKER}" ]; then
+        exec genmc -disable-spin-assume -check-liveness -imm $@
+else
+        OPTS="-it --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) -w $(pwd)"
+        exec docker run ${OPTS} cna-genmc scripts/genmc.sh $@
+fi
