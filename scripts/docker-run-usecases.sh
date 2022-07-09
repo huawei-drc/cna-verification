38d7b858b3c4b36c4522217299cab22ea3d0f7d0
Antonio Paolillo 00540335 <antonio.paolillo@huawei.com>
2022-07-09 11:57:13 +0200

    Update readme and timings in run script
    Signed-off-by: Antonio Paolillo 00540335 <antonio.paolillo@huawei.com>

--- scripts/docker-run-usecases.sh
+++ scripts/docker-run-usecases.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 set -ex
 
+# Usage (from root of the git repo): ./scripts/docker-run-usecases.sh
+# Notice this script assumes the "make prepared" step has been executed before.
+
 CNA_ALGORITHM=1
 QSPINLOCK_ALGORITHM=2
 
-make prepared
 make docker_build
 
 mkdir results
 export DOCKER=1
 
-# 01 verifying cna (original version) under IMM using GenMC, expect no error detected [duration ~10 seconds]
+# 01    Verifying cna (original version) under IMM using GenMC.
+#       Expect no error detected [duration ~10 seconds].
 ./scripts/genmc.sh cna-c11.c | tee results/out01-genmc-cna-c11.txt
 
-# 02 verifying qspinlock under LKMM using dartagnan, without applying any fix, expect to find a liveness violation [duration ~2 minutes]
+# 02    Verifying qspinlock under LKMM using Dartagnan,
+#       without applying any fix.
+#       Expect to find a liveness violation [duration ~1 minute].
 ./scripts/dartagnan.sh \
     -m lkmm \
     -p liveness \
@@ -21,7 +26,9 @@ export DOCKER=1
     -DSKIP_PENDING \
     client-code.c | tee results/out02-dartagnan-lkmm-qspinlock-livenessviolation.txt
 
-# 03 verifying qspinlock under LKMM using dartagnan, applying first fix, expect to find a safety violation [duration ~2 minutes]
+# 03    Verifying qspinlock under LKMM using Dartagnan,
+#       applying fix 1.
+#       Expect to find a safety violation [duration ~2 minutes].
 ./scripts/dartagnan.sh \
     -m lkmm \
     -p reachability,liveness \
@@ -30,7 +37,9 @@ export DOCKER=1
     -DFIX1 \
     client-code.c | tee results/out03-dartagnan-lkmm-qspinlock-safetyviolation.txt
 
-# 04 verifying qspinlock under LKMM using dartagnan, applying both fixes, expect no violation found and result UNKNOWN TODO [duration ~2 minutes]
+# 04    Verifying qspinlock under LKMM using Dartagnan,
+#       applying fixes 1 and 2.
+#       Expect no violation found and result [duration ~2 minutes].
 ./scripts/dartagnan.sh \
     -m lkmm \
     -p reachability,liveness \
@@ -39,7 +48,9 @@ export DOCKER=1
     -DFIX1 -DFIX2 \
     client-code.c | tee results/out04-dartagnan-lkmm-qspinlock-fixes.txt
 
-# 05 verifying qspinlock on Armv8, without fix, expect no violation found and result UNKNOWN TODO [duration ~XX minutes]
+# 05    Verifying qspinlock under Armv8 using Dartagnan,
+#       without applying any fix.
+#       Expect no violation found and result UNKNOWN [duration ~3 minutes].
 ./scripts/dartagnan.sh \
     -m armv8 \
     -p reachability,liveness \
@@ -47,7 +58,9 @@ export DOCKER=1
     -DSKIP_PENDING \
     client-code.c | tee results/out05-dartagnan-armv8-qspinlock.txt
 
-# 06 verifying qspinlock on Power, without fix, expect no violation found and result UNKNOWN TODO [duration ~XX minutes]
+# 06    Verifying qspinlock on Power using Dartagnan,
+#       without applying any fix.
+#       Expect no violation found and result UNKNOWN [duration ~3 minutes].
 ./scripts/dartagnan.sh \
     -m power \
     -p reachability,liveness \
@@ -55,7 +68,9 @@ export DOCKER=1
     -DSKIP_PENDING \
     client-code.c | tee results/out06-dartagnan-power-qspinlock.txt
 
-# 07 verifying CNA under LKMM using dartagnan, without applying any fix, expect to find a liveness violation [duration ~2 minutes]
+# 07    Verifying CNA under LKMM using Dartagnan,
+#       without applying any fix.
+#       Expect to find a liveness violation [duration ~30 minutes].
 ./scripts/dartagnan.sh \
     -m lkmm \
     -p liveness \
@@ -63,7 +78,9 @@ export DOCKER=1
     -DSKIP_PENDING \
     client-code.c | tee results/out07-dartagnan-lkmm-cna-livenessviolation.txt
 
-# 08 verifying CNA under LKMM using dartagnan, applying both fixes, expect no violation found and result UNKNOWN TODO [duration ~2 minutes]
+# 08    Verifying CNA under LKMM using Dartagnan,
+#       applying fixes 1 and 2.
+#       Expect no violation found and result UNKNOWN [duration ~16 hours].
 ./scripts/dartagnan.sh \
     -m lkmm \
     -p reachability,liveness \
@@ -72,7 +89,9 @@ export DOCKER=1
     -DFIX1 -DFIX2 \
     client-code.c | tee results/out08-dartagnan-lkmm-cna-fixes.txt
 
-# 09 verifying CNA on Armv8, without fix, expect no violation found and result UNKNOWN TODO [duration ~XX minutes]
+# 09    Verifying CNA on Armv8 using Dartagnan,
+#       without applying any fix.
+#       Expect no violation found and result UNKNOWN [duration ~12 hours].
 ./scripts/dartagnan.sh \
     -m armv8 \
     -p reachability,liveness \
@@ -80,7 +99,9 @@ export DOCKER=1
     -DSKIP_PENDING \
     client-code.c | tee results/out09-dartagnan-armv8-cna.txt
 
-# 10 verifying qspinlock on Power, without fix, expect no violation found and result UNKNOWN TODO [duration ~XX minutes]
+# 10    Verifying qspinlock on Power using Dartagnan,
+#       without applying any fix.
+#       Expect no violation found and result UNKNOWN [duration ~12 hours].
 ./scripts/dartagnan.sh \
     -m power \
     -p reachability,liveness \
